a74ef43daabe2c82d6c22b65ba9f1236377f3cbb
Brett Porter <brett@maestrodev.com>
2013-07-10 09:40:09 +1000

    improve path handling

--- lib/maestro_jenkins_exporter/version.rb
+++ lib/maestro_jenkins_exporter/version.rb
@@ -1,7 +1,7 @@
 require 'nokogiri'
 
 module MaestroJenkinsExporter
-  f = File.expand_path(File.join(File.dirname(__FILE__), "../../pom.xml"))
+  f = File.expand_path(File.join(File.dirname(__FILE__), '..', '..', 'pom.xml'))
   pom = Nokogiri::XML(IO.read(f))
   VERSION = pom.at_xpath('/xmlns:project/xmlns:version').text.gsub('-SNAPSHOT','.snapshot')
 end
